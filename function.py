f691d2c73ddf272b522493cf23391204c58a2655
Umair Noor <umairnoor878@gmail.com>
2025-03-13 04:03:29 +0500

    New

--- function.py
+++ function.py
@@ -12,3 +12,23 @@ def sum(a,b):
 
 final = sum(2,1)
 print(final) 
+
+#Function with Lambda 
+cube = lambda x: x**2
+print(cube(3))
+
+#Function with args 
+# def sum_all(*args):
+#     print(args)
+#     for i in args:
+#         print(i**2)
+#     return sum(args)
+
+# print(sum_all(1,2,3,4))
+
+import builtins  # Import Python's built-in functions
+
+def sum_all(*args):
+    return builtins.sum(args)  # Use the built-in sum()
+
+print(sum_all(1, 2, 3, 4))  # Output: 10
